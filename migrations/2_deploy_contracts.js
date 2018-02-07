473058057066394edc56e8c3cc32af75f5b98686
Mike Calvanese <mfcalvanese@gmail.com>
2018-02-06 19:44:41 -0500

    no mo' shrimp

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
 /* global artifacts */
 
-const ShrimpCoin = artifacts.require('ShrimpCoin')
 const Voting = artifacts.require('Voting')
 
 module.exports = function (deployer) {
   deployer.deploy(
-    ShrimpCoin,
     Voting
   )
 }
